7eaa16d8d20bdc22873001d95f4c212342468342
Alon Shalev Housfater <alonsh@ca.ibm.com>
2019-08-15 13:08:04 -0400

    Use Cleaner to free digest native context
    Signed-off-by: Alon Shalev Housfater <alonsh@ca.ibm.com>

--- src/java.base/share/classes/sun/security/provider/SunEntries.java
+++ src/java.base/share/classes/sun/security/provider/SunEntries.java
@@ -86,12 +86,11 @@ public final class SunEntries {
     /*
      * Check whether native crypto is enabled with property.
      * By default, the native crypto is enabled and uses native library crypto.
-     * The native crypto for MessageDigest is disabled until
-     * https://github.com/eclipse/openj9/issues/5611 can be resolved.
-     * The property 'jdk.nativeCrypto' is used to disable all native cryptos (Digest,
+     * The property 'jdk.nativeDigest' is used to disable Native digest alone
+     * and 'jdk.nativeCrypto' is used to disable all native cryptos (Digest,
      * CBC, GCM, and RSA).
      */
-    private static boolean useNativeDigest = false;
+    private static boolean useNativeDigest = true;
 
     // create an aliases List from the specified aliases
     public static List<String> createAliases(String ... aliases) {
